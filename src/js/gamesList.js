bcc6c080177071337f80bd367eb02b27cc2bd0b0
Mark29032011 <igromalcik@gmail.com>
2025-04-10 20:04:31 +0200

    !!!!

---
+++ src/js/gamesList.js
@@ -0,0 +1,25 @@
+const gameContainer = document.querySelector(".js-games")
+
+const games = [
+    {
+        id: 1,
+        name:'Name-1'
+
+    },
+    {
+        id:2,
+        name:'Name-2'
+    },
+    {
+        id:3,
+        name:'Name-3'
+    }
+]
+
+const list = games.map((game) => {
+    return `<li id=${game.id}>
+    <h2>${game.name}</h2>
+    </li>`
+
+})
+gameContainerEl.insertAdjacentHTML('afterend', list)
