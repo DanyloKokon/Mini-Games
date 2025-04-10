ff07b1e2901889ae519a251518feb88f0641a7c8
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2025-04-10 19:56:02 +0000

    Deploying to gh-pages from @ DanyloKokon/Mini-Games@3fe8e8e80553ca20a12bfe3ff001e4436a0e6500 🚀

---
+++ index.3020a57c.js
@@ -0,0 +1,2 @@
+document.querySelector(".burger-menu").addEventListener("click",(()=>{const e=document.querySelector(".nav-menu");e.style.display="none"===e.style.display||""===e.style.display?"block":"none"}));
+//# sourceMappingURL=index.3020a57c.js.map
---
+++ index.7a57cdd8.js
@@ -0,0 +1,2 @@
+document.querySelector(".burger-menu").addEventListener("click",(function(){var e=document.querySelector(".nav-menu");e.style.display="none"===e.style.display||""===e.style.display?"block":"none"}));
+//# sourceMappingURL=index.7a57cdd8.js.map
