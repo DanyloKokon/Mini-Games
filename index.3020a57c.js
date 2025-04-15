f36cd179a071ca336ed5d96316d4e9ab30cbbe79
IrynaSokolova-Quanandrya <80633345+IrynaSokolova-Quanandrya@users.noreply.github.com>
2025-04-15 22:36:23 +0000

    Deploying to gh-pages from @ DanyloKokon/Mini-Games@907b3ffafc10db04975e8c1380109101a4d32c50 🚀

--- index.3020a57c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".burger-menu").addEventListener("click",(()=>{const e=document.querySelector(".nav-menu");e.style.display="none"===e.style.display||""===e.style.display?"block":"none"}));
-//# sourceMappingURL=index.3020a57c.js.map
--- index.7a57cdd8.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".burger-menu").addEventListener("click",(function(){var e=document.querySelector(".nav-menu");e.style.display="none"===e.style.display||""===e.style.display?"block":"none"}));
-//# sourceMappingURL=index.7a57cdd8.js.map
