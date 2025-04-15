ae5100a5bcb705d227a7eeaaa7b579f38855b2c7
Mark29032011 <igromalcik@gmail.com>
2025-04-15 19:45:47 +0200

    rsp game

--- src/js/gameRSP.js
+++ src/js/gameRSP.js
@@ -1,2 +1,12 @@
 const gameContainerEl = document.getElementById('3')
-gameContainerEl.insertAdjacentHTML('afterend', `<h4>Камінь - ножиці - папір<h4>`)
+const title = document.createElement('h2');
+title.innerText = 'Камінь - ножиці - папір';
+document.body.appendChild(title);
+
+const choices = ['stone', 'scissors', 'paper'];
+choices.forEach(choice => {
+    const  button = document.createElement('button');
+    button.innerText = choice.charAt(0).toUpperCase() + choice.slice(1);
+    button.onClick = () => play(choice);
+    document.body.appendChild(button);
+});
