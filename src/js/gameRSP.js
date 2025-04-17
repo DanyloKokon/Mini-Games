cdb6cc4a25567cf0e8c3a66c23cc1c5798c23ff8
Mark29032011 <igromalcik@gmail.com>
2025-04-17 19:16:10 +0200

    rsp mark

--- src/js/gameRSP.js
+++ src/js/gameRSP.js
@@ -1,12 +1,40 @@
-const gameContainerEl = document.getElementById('3')
+const gameContainer = document.getElementById('3')
 const title = document.createElement('h2');
-title.innerText = 'Камінь - ножиці - папір';
+title.textContent = 'Камінь - ножиці - папір';
 document.body.appendChild(title);
 
-const choices = ['stone', 'scissors', 'paper'];
+const choices = ['Камінь', 'ножиці', 'папір'];
+let playerScore = 0;
+let computerScore = 0;
+
+const btnContainer = document.createElement('div');
+document.body.appendChild(btnContainer)
+
 choices.forEach(choice => {
-    const  button = document.createElement('button');
-    button.innerText = choice.charAt(0).toUpperCase() + choice.slice(1);
-    button.onClick = () => play(choice);
-    document.body.appendChild(button);
+   const btn = document.createElement('button');
+   btn.textContent = choice;
+   btn.addEventListener('click', () => play(choice));
+   btnContainer.appendChild(btn);
 });
+
+const result = document.createElement('div');
+result.className = 'result';
+document.body.appendChild(result);
+
+const score = document.createElement('div');
+score.className = 'score';
+const computerScoreSpan = document.createElement('span');
+const playerScoreSpan = document.createElement('span');
+computerScoreSpan.id = 'computerScore';
+playerScoreSpan.id = 'playerScore';
+computerScoreSpan.textContent = '0';
+playerScoreSpan.textContent = '0';
+score.innerHTML = `Рахунок: <br> Комп'ютер: `;
+score.appendChild(computerScoreSpan);
+score.innerHTML += ' | Ви: ';
+score.appendChild(playerScoreSpan);
+document.body.appendChild(score);
+// '''''''''''''Логикa''''''''''''''''''''''''
+// function play(playerChoice) {
+//     const computerChoice = choices
+// }
