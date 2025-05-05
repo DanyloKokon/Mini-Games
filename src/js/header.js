86f44823900aeec9e94ec30cc6536c2b8953b55e
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2025-05-05 19:12:15 +0200

    івапрола

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,7 @@
+const color = document.querySelector("#color-change");
 
-
-
+color.addEventListener('click', () => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme');
+   
+})
