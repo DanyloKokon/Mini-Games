342ba27f779485bcb6b74a2e8acb7846f06524a8
PryhodiiMia <149527041+PryhodiiMia@users.noreply.github.com>
2025-05-06 19:29:33 +0300

    add change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./js/header";
 import "./js/gamesList";
 import "./js/gameRSP";
 import "./js/yobc";
+import "./js/GTNTCG";
 import "./js/calculator";
-import "./js/footer"
-import "./js/dino"
+import "./js/footer";
+import "./js/dino";
